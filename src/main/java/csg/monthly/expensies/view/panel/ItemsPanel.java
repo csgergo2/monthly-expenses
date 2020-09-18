c0079f0e765a7854809a23321f9ba2399a52e20d
csgergo <csgergocs@gmail.com>
2020-09-18 18:41:22 +0200

    separated outgoings and incomings in items panel

--- src/main/java/csg/monthly/expensies/view/panel/ItemsPanel.java
+++ src/main/java/csg/monthly/expensies/view/panel/ItemsPanel.java
@@ -2,14 +2,16 @@ package csg.monthly.expensies.view.panel;
 
 import static csg.monthly.expensies.view.util.Name.ITEMS_BACK_BUTTON;
 import static csg.monthly.expensies.view.util.Name.ITEMS_CALCULATE_TABLE_BUTTON;
+import static csg.monthly.expensies.view.util.Name.ITEMS_INCOMES_TABLE;
 import static csg.monthly.expensies.view.util.Name.ITEMS_MONTH_SELECTOR;
-import static csg.monthly.expensies.view.util.Name.ITEMS_TABLE;
+import static csg.monthly.expensies.view.util.Name.ITEMS_OUTGOINGS_TABLE;
 import static csg.monthly.expensies.view.util.Name.ITEMS_YEAR_SELECTOR;
 
 import java.awt.event.ActionEvent;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import csg.monthly.expensies.Application;
 import csg.monthly.expensies.domain.Item;
@@ -37,7 +39,8 @@ public class ItemsPanel extends CsGPanel {
 
     private CsGComboBox<Integer> yearSelector = new CsGComboBox<>(ITEMS_YEAR_SELECTOR);
     private CsGComboBox<Month> monthSelector = createMonthSelector();
-    private CsGScrollableLabel table = new CsGScrollableLabel(ITEMS_TABLE, "");
+    private CsGScrollableLabel tableOfOutgoings = new CsGScrollableLabel(ITEMS_OUTGOINGS_TABLE, "");
+    private CsGScrollableLabel tableOfIncomes = new CsGScrollableLabel(ITEMS_INCOMES_TABLE, "");
 
     private ItemsPanel() {
         super(Name.ITEMS_PANEL, MELayout.LAYOUT);
@@ -48,7 +51,8 @@ public class ItemsPanel extends CsGPanel {
         add(new CsGButton(ITEMS_BACK_BUTTON, "Vissza", this::backToMenuPanel));//todo english
         add(new CsGButton(ITEMS_CALCULATE_TABLE_BUTTON, "Mutasd", this::calculateItemsTable));//todo english
 
-        add(table);
+        add(tableOfOutgoings);
+        add(tableOfIncomes);
     }
 
     @Override
@@ -64,14 +68,26 @@ public class ItemsPanel extends CsGPanel {
     private void calculateItemsTable(ActionEvent event) {
         ItemRepository itemRepository = Application.getBean(ItemRepository.class);
         List<Item> items = itemRepository.findAllByYearAndMonth((int) yearSelector.getSelectedItem(), (Month) monthSelector.getSelectedItem());
-        String[][] rows = new String[items.size()][4];
-        for (int i = 0; i < items.size(); i++) {
-            rows[i][0] = items.get(i).getDate().toLocalDate().format(DATE_TIME_FORMATTER);
-            rows[i][1] = items.get(i).getName();
-            rows[i][2] = Integer.toString(items.get(i).getAmount());
-            rows[i][3] = items.get(i).getTag().getName();
+        //outgoings
+        List<Item> outgoings = items.stream().filter(item -> !item.isIncome()).collect(Collectors.toList());
+        String[][] rows = new String[outgoings.size()][4];
+        for (int i = 0; i < outgoings.size(); i++) {
+            rows[i][0] = outgoings.get(i).getDate().toLocalDate().format(DATE_TIME_FORMATTER);
+            rows[i][1] = outgoings.get(i).getName();
+            rows[i][2] = Integer.toString(outgoings.get(i).getAmount());
+            rows[i][3] = outgoings.get(i).getTag().getName();
         }
-        table.setText(CsGHtmlBuilder.createHtmlTable(TABLE_HEADERS, rows));
+        tableOfOutgoings.setText(CsGHtmlBuilder.createHtmlTable(TABLE_HEADERS, rows));
+        //incomes
+        List<Item> incomes = items.stream().filter(Item::isIncome).collect(Collectors.toList());
+        rows = new String[incomes.size()][4];
+        for (int i = 0; i < incomes.size(); i++) {
+            rows[i][0] = incomes.get(i).getDate().toLocalDate().format(DATE_TIME_FORMATTER);
+            rows[i][1] = incomes.get(i).getName();
+            rows[i][2] = Integer.toString(incomes.get(i).getAmount());
+            rows[i][3] = incomes.get(i).getTag().getName();
+        }
+        tableOfIncomes.setText(CsGHtmlBuilder.createHtmlTable(TABLE_HEADERS, rows));
     }
 
     private void backToMenuPanel(ActionEvent event) {
