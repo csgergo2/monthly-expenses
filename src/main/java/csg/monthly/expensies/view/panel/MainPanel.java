890ace3f916a8d1c71baeb94b98e9fa4313201e0
csgergo <csgergocs@gmail.com>
2020-10-09 00:44:45 +0200

    added MonthlySumPanel base and navigating between panels

--- src/main/java/csg/monthly/expensies/view/panel/MainPanel.java
+++ src/main/java/csg/monthly/expensies/view/panel/MainPanel.java
@@ -1,8 +1,10 @@
 package csg.monthly.expensies.view.panel;
 
 import static csg.monthly.expensies.view.panel.ItemsPanel.ITEMS_PANEL;
+import static csg.monthly.expensies.view.panel.MenuPanel.MENU_PANEL;
 import static csg.monthly.expensies.view.panel.NewItemPanel.NEW_ITEM_PANEL;
 import static csg.monthly.expensies.view.panel.NewTagPanel.NEW_TAG_PANEL;
+import static csg.monthly.expensies.view.util.Name.ITEMS_BACK_BUTTON;
 import static csg.monthly.expensies.view.util.Name.MAIN_PANEL_REFRESH_BUTTON;
 
 import java.awt.event.ActionEvent;
@@ -22,12 +24,18 @@ public class MainPanel extends CsGPanel {
         add(NEW_ITEM_PANEL);
         add(NEW_TAG_PANEL);
         add(new CsGButton(MAIN_PANEL_REFRESH_BUTTON, "Frissítés", this::refresh));//todo english
+        add(new CsGButton(ITEMS_BACK_BUTTON, "Vissza", this::back));//todo english
     }
 
     private void refresh(ActionEvent event) {
         ITEMS_PANEL.refresh();
         NEW_ITEM_PANEL.refresh();
         NEW_TAG_PANEL.refresh();
+    }
+
+    private void back(ActionEvent event) {
+        setVisible(false);
+        MENU_PANEL.setVisible(true);
     }
 
     @Override
