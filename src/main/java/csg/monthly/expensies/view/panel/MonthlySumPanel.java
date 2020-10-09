150ce7490cc30fe9ed5f2fc0c5d8e089fb516cd4
csgergo <csgergocs@gmail.com>
2020-10-09 14:55:44 +0200

    added tag table for monthly sum

--- src/main/java/csg/monthly/expensies/view/panel/MonthlySumPanel.java
+++ src/main/java/csg/monthly/expensies/view/panel/MonthlySumPanel.java
@@ -3,9 +3,13 @@ package csg.monthly.expensies.view.panel;
 import static csg.monthly.expensies.view.panel.MenuPanel.MENU_PANEL;
 import static csg.monthly.expensies.view.util.Name.MONTHLY_SUM_BACK_BUTTON;
 import static csg.monthly.expensies.view.util.Name.MONTHLY_SUM_MAIN_LABEL;
+import static csg.monthly.expensies.view.util.Name.MONTHLY_SUM_TAGS_LABEL;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.AdjustmentEvent;
 import java.util.Random;
+
+import javax.swing.JScrollBar;
 
 import csg.monthly.expensies.view.util.MELayout;
 import csg.monthly.expensies.view.util.Name;
@@ -17,6 +21,7 @@ public class MonthlySumPanel extends CsGPanel {
 
     public static final MonthlySumPanel MONTHLY_SUM_PANEL = new MonthlySumPanel();
 
+    private CsGScrollableLabel tags = new CsGScrollableLabel(MONTHLY_SUM_TAGS_LABEL, "");
     private CsGScrollableLabel table = new CsGScrollableLabel(MONTHLY_SUM_MAIN_LABEL, "");
 
     private MonthlySumPanel() {
@@ -34,10 +39,27 @@ public class MonthlySumPanel extends CsGPanel {
         sb.append("</html>");
         table.setText(sb.toString());
         add(table);
+
+        sb = new StringBuilder("<html>");
+        for (int i = 0; i < 100; i++) {
+            sb.append(new Random().nextInt());
+            sb.append("<br/>");
+        }
+        sb.append("</html>");
+        tags.setText(sb.toString());
+        tags.getVerticalScrollBar().addAdjustmentListener(this::changeTagsScroll);
+        add(tags);
     }
 
     private void back(ActionEvent event) {
         setVisible(false);
         MENU_PANEL.setVisible(true);
+    }
+
+    private void changeTagsScroll(AdjustmentEvent event) {
+        if (!event.getValueIsAdjusting()) {
+            JScrollBar source = (JScrollBar) event.getAdjustable();
+            table.getVerticalScrollBar().getModel().setValue(source.getModel().getValue());
+        }
     }
 }
