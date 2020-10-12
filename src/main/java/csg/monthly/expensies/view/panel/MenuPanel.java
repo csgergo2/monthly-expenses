089f5279c6a1771ca73b783e443df6831c341091
csgergo <csgergocs@gmail.com>
2020-10-12 20:06:21 +0200

    prepare to 1.1.6-SNAPSHOT and fix after merge

--- src/main/java/csg/monthly/expensies/view/panel/MenuPanel.java
+++ src/main/java/csg/monthly/expensies/view/panel/MenuPanel.java
@@ -2,10 +2,8 @@ package csg.monthly.expensies.view.panel;
 
 import static csg.monthly.expensies.view.panel.FilteringPanel.FILTERING_PANEL;
 import static csg.monthly.expensies.view.panel.MainPanel.MAIN_PANEL;
-import static csg.monthly.expensies.view.panel.MonthlySumPanel.MONTHLY_SUM_PANEL;
 import static csg.monthly.expensies.view.util.Name.MENU_PANEL_FILTERING_PANEL_BUTTON;
 import static csg.monthly.expensies.view.util.Name.MENU_PANEL_START_BUTTON;
-import static csg.monthly.expensies.view.util.Name.MONTHLY_SUM_PANEL_BUTTON;
 
 import java.awt.event.ActionEvent;
 
